07ad6e1d5ca540558e82b6c058b328127e05a500
fedorenkokhalilov <fedorenkokhalilov@gmail.com>
2016-10-03 13:15:04 +0300

    Add files via upload

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BoardController : MonoBehaviour {
+
+
+	private Image spriteRenderer;
+
+	private PlayerController playerStatus;
+
+	void Start () {
+		spriteRenderer = this.gameObject.GetComponent<Image> ();
+		playerStatus = FindObjectOfType<PlayerController> ();
+	}
+	
+	public void ChangeToXorO () {
+		if (playerStatus.XO == PlayerController.PlyerSide.X) {
+			spriteRenderer.sprite = playerStatus.PlayerList [0];
+			playerStatus.stat = PlayerController.StatusOfGame.Play;
+		}
+		if (playerStatus.XO == PlayerController.PlyerSide.O) {
+			spriteRenderer.sprite = playerStatus.PlayerList [1];
+			playerStatus.stat = PlayerController.StatusOfGame.Play;
+		}
+
+	}
+
+	void Update(){
+		if ((playerStatus.XO == PlayerController.PlyerSide.X||playerStatus.XO == PlayerController.PlyerSide.O) && playerStatus.stat== PlayerController.StatusOfGame.Wait) {
+			spriteRenderer.sprite = playerStatus.PlayerList [2];
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+	public Sprite[] PlayerList;
+
+	private SpriteRenderer spriteRenderer;
+
+	public enum StatusOfGame{
+		Play,
+		Wait,
+		ChuseSide
+	}
+
+	public enum PlyerSide
+	{
+		None,
+		X,
+		O
+	}
+	public PlyerSide XO;
+	public StatusOfGame stat;
+
+	void Awake () {
+		spriteRenderer = this.gameObject.GetComponent<SpriteRenderer> ();
+		stat = StatusOfGame.ChuseSide;
+	}
+
+	public void XChose () {
+	XO = PlyerSide.X;
+	stat = StatusOfGame.Wait;
+	}
+
+	public void OChose () {
+	XO = PlyerSide.O;
+		stat = StatusOfGame.Wait;
+	}
+
+	public void NoneChose () {
+		XO = PlyerSide.None;
+		spriteRenderer.sprite = null;
+		stat = StatusOfGame.ChuseSide;
+	}
+
+	private void Update()
+	{
+		if (XO == PlyerSide.X) {
+			spriteRenderer.sprite = PlayerList [0];
+		}
+		if (XO == PlyerSide.O) {
+			spriteRenderer.sprite = PlayerList [1];
+		}
+	}
+}
